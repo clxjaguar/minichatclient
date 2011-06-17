90178cd7f280734e8b27fa10b06b24955798d171
Niki <niki@nikiroo.be>
2011-06-17 14:38:10 +0000

    Correction for parser.c and new test "parser-test.c"

---
+++ parser-test.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "parser.h"
+
+int main(int argc, char *argv[]) {
+	char *test;
+	
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s [HTML string to parse]\n", argv[0]);
+		return 1;
+	}
+	
+	test = parse_html_for_output(argv[1]);
+	printf("%s\n", test);
+	
+	return 0;
+}
