60c2c37a6d75a5b9e62990aaf006be86d2472cdf
Niki <niki@nikiroo.be>
2011-06-22 11:06:43 +0000

    parser.c now uses cstring and clist.

--- parser_p.h
+++ parser_p.h
@@ -5,43 +5,24 @@
 #define TYPE_OPENING_TAG  1
 #define TYPE_CLOSING_TAG  2
 
-/**
- * This is an attribute (a key-value pair).
- * You should use attribute instead of this struct.
- * Note that the name cannot be NULL, but the value can.
- * Edit (cLx) : 
- * [NOTE : THIS SECTION HAS BEEN COPIED FROM INI.H]
- */
-struct attribute_struct {
-	char *name;
-	char *value;
-};
+#include "attribute.h"
+#include "clist.h"
 
-/**
- * This is an attribute (a key-value pair).
- */
-typedef struct attribute_struct attribute;
-
-/*
-[END OF THE COPY'D SECTION]
-*/
-
-typedef struct message_part message_part_t;
-struct message_part {
+typedef struct message_part_struct message_part;
+struct message_part_struct {
 	int type; // (0 or 1 or 2: 0 is text, 1 is <>, 2 is </>)
 	char *data;
-	message_part_t *link; // (NULL for text)
-	attribute **attributes; // (NULL for text)
+	message_part *link; // (NULL for text)
+	clist *attributes; // (NULL for text)
 };
 
 char *get_date();
-message_part_t **get_parts(char *message);
-char *get_text(message_part_t *message);
+clist *get_parts(char *message);
+char *get_text(message_part *message);
 void configure();
-char *add_char(char *dest, char car);
-message_part_t **message_part_add_to_list(message_part_t **list, message_part_t *part);
-message_part_t *process_part(char *data, int text);
-void free_message_part(message_part_t* message);
-void free_message_parts(message_part_t** messages);
+clist_node *process_part(char *data, int text);
+void free_message_part(message_part* message);
+void free_message_parts(message_part** messages);
+void free_message_part_node(clist_node* node);
 
 #endif
