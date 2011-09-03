a34ec56af4177aed970d92761973950c9d9391b2
nikiroo <nikiroo@caf73ff4-54bb-40f4-97cd-72ea6ff03f61>
2011-09-03 10:00:42 +0000

    Test improved for cstring-test (they are now non-interactive).

--- cstring-test.c
+++ cstring-test.c
@@ -3,68 +3,97 @@
 #include "cstring.h"
 #include "clist.h"
 
-void do_operations(int verbose);
-void print_cstring_list(clist *list);
+#define boolean int
+#define true 1
+#define false 0
 
-void print_cstring_list(clist *list) {
-	clist_node *node;
+boolean test(const char *name, const char *expected, const char *value, boolean verbose);
+boolean test_i(const char *name, int expected, int value, boolean verbose);
+
+boolean do_operations(boolean verbose);
+
+boolean test(const char *name, const char *expected, const char *value, boolean verbose) {
+	boolean result = true;
 	
-	printf("Printing list:\n{\n");
-	for (node = list->first ; node != NULL ; node = node->next) {
-		printf(" %s\n", ((cstring *)node->data)->string);
+	if (verbose) {
+		printf("*** %25.25s     ..........     ", name);
+		fflush(NULL);
 	}
-	printf("}\n");
+	
+	result = !strcmp(value, expected);
+	
+	if (verbose) {
+		printf((result ? "OK\n" : "KO\n"));
+		if (!result) {
+			printf("'%s' should have been '%s'\n", value, expected);
+		}
+		fflush(NULL);
+	}
+	
+	return result;
+}
+
+boolean test_i(const char *name, int expected, int value, boolean verbose) {
+	boolean result = true;
+	
+	if (verbose) {
+		printf("*** %25.25s     ..........     ", name);
+		fflush(NULL);
+	}
+	
+	result = (value == expected);
+	
+	if (verbose) {
+		printf((result ? "OK\n" : "KO\n"));
+		if (!result) {
+			printf("'%i' should have been '%i'\n", value, expected);
+		}
+	}
+	
+	return result;
 }
 
 int main (int argc, char **argv) {
 	unsigned long i;
+	boolean result;
 
-	do_operations(1);
+	result = do_operations(true);
 
-	// Check for memory leaks/check perfs
-	printf("Running 100.000 operations...\n");
-	for (i = 0 ; i < 100 * 1000 ; i++ ){
-		do_operations(0);
+	if (result) {
+		// Check for memory leaks/check perfs
+		printf("*** %25.25s     ..........     ", "Running 100.000 operations");
+		fflush(NULL);
+		for (i = 0 ; result && i < 100 * 1000 ; i++ ){
+			result = result && do_operations(false);
+		}
+		printf((result ? "OK\n" : "KO\n"));
 	}
-	printf("Done.\n");
 	
-	return 0;
+	return !result;
 }
 
-void do_operations(int verbose) {
+boolean do_operations(boolean verbose) {
 	cstring *string, *string2, *string3;
 	char *s2;
 	clist *split_list;
-	int b;
+	boolean b;
+	boolean result = true;
+	clist_node *node;
 
 	string = new_cstring();
-	if (verbose) {
-		printf("Empty cstring: '%s'\n", string->string);
-	}
+	result = result && test("Empty string", "", string->string, verbose);
 	cstring_reverse(string);
-	if (verbose) {
-		printf("Reverse (still empty): '%s'\n", string->string);
-	}
+	result = result && test("Reverse empty string", "", string->string, verbose);
 	cstring_adds(string, "Test.");
-	if (verbose) {
-		printf("Add 'Test.': '%s'\n", string->string);
-	}
+	result = result && test("adds", "Test.", string->string, verbose);
 	cstring_clear(string);
-	if (verbose) {
-		printf("Clear it (empty cstring): '%s'\n", string->string);
-	}
+	result = result && test("clear", "", string->string, verbose);
 	cstring_adds(string, "0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 Test.");
-	if (verbose) {
-		printf("Adds 8 times 0->9, space, then ' Test.': '%s'\n", string->string);
-	}
+	result = result && test("adds", "0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 Test.", string->string, verbose);
 	cstring_reverse(string);
-	if (verbose) {
-		printf("Reverse it: '%s'\n", string->string);
-	}
+	result = result && test("reverse non-empty string", ".tseT 9876543210 9876543210 9876543210 9876543210 9876543210 9876543210 9876543210 9876543210", string->string, verbose);
 	cstring_addi(string, 98);
-	if (verbose) {
-		printf("Add '98'  : '%s'\n", string->string);
-	}
+	result = result && test("addi", ".tseT 9876543210 9876543210 9876543210 9876543210 9876543210 9876543210 9876543210 987654321098", string->string, verbose);
 	free_cstring(string);
 	
 	string = new_cstring();
@@ -80,71 +109,64 @@ void do_operations(int verbose) {
 	cstring_add(string, string2);
 	free_cstring(string2);
 
-	if (verbose) {
-		printf("You should see 'String 2': '%s'\n", string->string);
-		printf("You should see 'String 2': '%s'\n", *(char **)string);
-	}
+	result = result && test("addi/addc/adds", "String 2", string->string, verbose);
+	result = result && test("char **conversions", "String 2", *(char **)string, verbose);
 
 	cstring_clear(string);
 	cstring_addns(string, "1234567890", 4);
-	if (verbose) {
-		printf("You should see 1234: '%s'\n", string->string);
-	}
+	result = result && test("addns", "1234", string->string, verbose);
 	cstring_clear(string);
 	cstring_adds(string, "1234567890");
 	cstring_cut_at(string, 4);
-	if (verbose) {
-		printf("You should see 1234: '%s'\n", string->string);
-	}
+	result = result && test("cut_at", "1234", string->string, verbose);
 	cstring_clear(string);
 	cstring_addfs(string, "1234567890", 4);
-	if (verbose) {
-		printf("You should see 567890: '%s'\n", string->string);
-	}
+	result = result && test("addfs", "567890", string->string, verbose);
 	cstring_clear(string);
 	cstring_addfns(string, "1234567890", 4, 4);
-	if (verbose) {
-		printf("You should see 5678: '%s'\n", string->string);
-	}
+	result = result && test("addfns", "5678", string->string, verbose);
 	cstring_clear(string);
 	cstring_addi(string, 12);
-	if (verbose) {
-		printf("You should see 12: '%s'\n", string->string);
-	}
+	result = result && test("addi", "12", string->string, verbose);
 	cstring_addx(string, 0xFF2);
-	if (verbose) {
-		printf("You should see 12ff2: '%s'\n", string->string);
-	}
+	result = result && test("addx", "12ff2", string->string, verbose);
 	cstring_clear(string);
 	cstring_addX(string, 0xABAC);
-	if (verbose) {
-		printf("You should see ABAC: '%s'\n", string->string);
-	}
+	result = result && test("addX", "ABAC", string->string, verbose);
 	cstring_reverse(string);
-	if (verbose) {
-		printf("You should see CABA: '%s'\n", string->string);
-	}
+	result = result && test("reverse non-empty string", "CABA", string->string, verbose);
 	cstring_clear(string);
 	cstring_adds(string, "12 34  '56 78'");
+	
 	split_list = cstring_splitc(string, ' ', '\'');
-	if (verbose) {
-		printf("You should see 12, then 34, then '', then '56 78':\n");
-		print_cstring_list(split_list);
-	}
+	node = split_list->first;
+	result = result && test("splitc 1", "12", ((cstring *)node->data)->string, verbose);
+	node = node->next;
+	result = result && test("splitc 1", "34", ((cstring *)node->data)->string, verbose);
+	node = node->next;
+	result = result && test("splitc 1", "", ((cstring *)node->data)->string, verbose);
+	node = node->next;
+	result = result && test("splitc 1", "56 78", ((cstring *)node->data)->string, verbose);
+	result = result && test_i("splitc 1", true, node->next == NULL, verbose);
+	
 	free_clist(split_list);
 	split_list = cstring_splitc(string, 'Z', '\0');
-	if (verbose) {
-		printf("You should see \"12 34  '56 78'\":\n");
-		print_cstring_list(split_list);
-	}
+	node = split_list->first;
+	result = result && test("splitc 2", "12 34  '56 78'", ((cstring *)node->data)->string, verbose);
+	result = result && test_i("splitc 2", true, node->next == NULL, verbose);
+	
 	free_clist(split_list);
 	cstring_clear(string);
 	cstring_adds(string, "\"12=34\"=56=\"78=90\"");
 	split_list = cstring_splitc(string, '=', '\"');
-	if (verbose) {
-		printf("You should see \"12=34 56 78=90\":\n");
-		print_cstring_list(split_list);
-	}
+	node = split_list->first;
+	result = result && test("splitc 3", "12=34", ((cstring *)node->data)->string, verbose);
+	node = node->next;
+	result = result && test("splitc 3", "56", ((cstring *)node->data)->string, verbose);
+	node = node->next;
+	result = result && test("splitc 3", "78=90", ((cstring *)node->data)->string, verbose);
+	result = result && test_i("splitc 3", true, node->next == NULL, verbose);
+	
 	free_clist(split_list);
 	cstring_clear(string);
 	cstring_adds(string, "12345 67890");
@@ -153,9 +175,7 @@ void do_operations(int verbose) {
 	cstring_adds(string2, "78");
 	cstring_adds(string3, "__");
 	cstring_replace(string, string2, string3);
-	if (verbose) {
-		printf("You should see '12345 6__90': '%s'\n", string->string);
-	}
+	result = result && test("replace", "12345 6__90", string->string, verbose);
 	cstring_clear(string);
 	cstring_clear(string2);
 	cstring_clear(string3);
@@ -163,9 +183,7 @@ void do_operations(int verbose) {
 	cstring_adds(string2, "78");
 	cstring_adds(string3, "_");
 	cstring_replace(string, string2, string3);
-	if (verbose) {
-		printf("You should see '12345 6_90': '%s'\n", string->string);
-	}
+	result = result && test("replace", "12345 6_90", string->string, verbose);
 	cstring_clear(string);
 	cstring_clear(string2);
 	cstring_clear(string3);
@@ -173,48 +191,34 @@ void do_operations(int verbose) {
 	cstring_adds(string2, "78");
 	cstring_adds(string3, "___");
 	cstring_replace(string, string2, string3);
-	if (verbose) {
-		printf("You should see '12345 6___90': '%s'\n", string->string);
-	}
+	result = result && test("replace", "12345 6___90", string->string, verbose);
 	free_cstring(string2);
 	free_cstring(string3);
 	cstring_clear(string);
 	cstring_adds(string, "1234567890");
 	string2 = cstring_substring(string, 3, 2);
-	if (verbose) {
-		printf("You should see '45': '%s'\n", string2->string);
-	}
+	result = result && test("replace", "45", string2->string, verbose);
 	free_cstring(string2);
 	cstring_clear(string);
 	cstring_adds(string, "1234567890");
 	b = cstring_starts_withs(string, "123", 0);
-	if (verbose) {
-		printf("'1234567890' starts with '123': %s\n", (b?"YES":"NO"));
-	}
+	result = result && test_i("starts_withs", true, b, verbose);
 	b = cstring_ends_withs(string, "890", 0);
-	if (verbose) {
-		printf("'1234567890' ends with '890': %s\n", (b?"YES":"NO"));
-	}
+	result = result && test_i("ends_withs", true, b, verbose);
 	b = cstring_starts_withs(string, "124, 0", 0);
-	if (verbose) {
-		printf("'1234567890' starts with '124': %s\n", (b?"YES":"NO"));
-	}
+	result = result && test_i("starts_withs", false, b, verbose);
 	b = cstring_ends_withs(string, "8900", 0);
-	if (verbose) {
-		printf("'1234567890' ends with '8900': %s\n", (b?"YES":"NO"));
-	}
+	result = result && test_i("ends_withs", false, b, verbose);
 	cstring_clear(string);
 	cstring_adds(string, " text text @@ ");
 	b = cstring_ends_withs(string, "@ ", 0);
-	if (verbose) {
-		printf("'%s' ends with '@ ': %s\n", string->string, (b?"YES":"NO"));
-	}
+	result = result && test_i("ends_withs", true, b, verbose);
 	cstring_clear(string);
 	cstring_adds(string, "@ ");
 	b = cstring_ends_withs(string, "@ ", 0);
-	if (verbose) {
-		printf("'%s' ends with '@ ': %s\n", string->string, (b?"YES":"NO"));
-	}
+	result = result && test_i("ends_withs", true, b, verbose);
 
 	free_cstring(string);
+	
+	return result;
 }
