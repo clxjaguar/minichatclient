08f7f762d33700618fc08c19551f6b4b4f5bd093
cLx <clx.kat@gmail.com>
2011-09-05 21:29:07 +0000

    a general code cleanup

--- parsehtml.h
+++ parsehtml.h
@@ -1,12 +1,9 @@
-#ifndef PARSEHTML_H_INCLUDED
-#define PARSEHTML_H_INCLUDED
-
 typedef struct {
-    char *username;
-    char *message;
-    char *usericonurl;
-    char *userprofileurl;
-    char msgid[30];
+	char *username;
+	char *message;
+	char *usericonurl;
+	char *userprofileurl;
+	char msgid[30];
 } message_t;
 
 int parser_freerules(void);
@@ -14,5 +11,3 @@ int parser_loadrules(void);
 unsigned int parse_minichat_mess(char *input, unsigned int bytes, message_t *msg, int reset);
 void minichat_message(char* username, char* message, char *usericonurl, char *userprofileurl);
 void minichat_users_at_this_moment(char *string);
-
-#endif // MESSAGE_H_INCLUDED
--- parser.h
+++ parser.h
@@ -1,83 +1,29 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "clist.h"
-
-#define TYPE_MESSAGE      0
-#define TYPE_OPENING_TAG  1
-#define TYPE_CLOSING_TAG  2
-
 typedef struct {
 	void *data;
 } parser_config;
 
-typedef struct parser_message_struct parser_message;
-struct parser_message_struct {
-	int type; // (0 or 1 or 2: 0 is text, 1 is <>, 2 is </>)
-	char *data;
-	parser_message *link; // (NULL for text)
-	clist *attributes; // (NULL for text)
-};
-
-typedef struct {
-	char *tag;
-	char *value;
-	char *start;
-	char *stop;
-} parser_rule;
-
-/**
- * Process and return the parser_messages found in this message.
- * They will be fully checked against all special rules and such.
- * Don't forget to call free_clist() on it when done.
- *
- * @param message the message to work on
- *
- * @return a list of parser_message's
- */
-clist *parser_get_parts(const char *message);
-
-/**
- * Parse the given message (HTML) and return a new, parsed string.
+/* Parse the given message (HTML) and return a clean, neat, text only version.
  * This is a helper function to parser_parse_messages() which also create
  * and return a string with the parsed message.
+ * AND PERMIT TO DON'T INCLUDE ANOTHER .H WITH THAT .H !!!
  *
  * @param message the message to parse
  * @param config the configuration to use to parse the parser_message's
- *
- * @return the parsed string
- */
-char *parse_html_in_a_single_message(const char *message, parser_config *config);
+ * @return the parsed string */
+char *parse_html_in_message(const char *message, parser_config *config);
 
-/**
- * Parse the given parser_message's in a separate, given function.
- * It will call (operation) on each parser_message, telling you which rule apply
- * to this part, if any.
- *
- * @param parts the parser_message's to parse
- * @param config the configuration to use to parse the parser_message's
- * @param operation the function to use to parse each parser_message
- * @param argument the user argument to pass to the function
- */
-void parser_parse_messages(clist *parts, parser_config *config, 
-	void (*operation)(parser_message *part, parser_rule *rul, void *argument),
-	void *argument);
-
-/**
- * Parse the given file for an output configuration.
+/* Parse the given file for an output configuration.
  * Don't forget free_parser_config().
  *
  * @param filename the file to open
- *
- * @return the configuration object
- */
+ * @return the configuration object */
 parser_config *parser_get_config(const char filename[]);
 
-/**
- * Free the parser_config.
+/* Free the parser_config.
  *
- * @param config the configuration object
- */
+ * @param config the configuration object */
 void free_parser_config(parser_config *config);
-
 #endif
