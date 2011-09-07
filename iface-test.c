35341dce666ffb6d3570aff711e375e184528737
nikiroo <nikiroo@caf73ff4-54bb-40f4-97cd-72ea6ff03f61>
2011-09-07 14:38:09 +0000

    Display interface test, text interface

---
+++ iface-test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "display_interfaces.h"
+#include "cstring.h"
+
+#define true 1
+#define false 0
+
+#define MAX 10000000
+
+int main (int argc, char **argv) {
+	// disable warnings
+	if (argv == 0) argc = argc;
+
+	size_t index;
+	char *str;
+
+printf("Will display...\n");
+	display_init();
+printf("Did display.\n");
+
+	index = 0;
+	while (index < MAX) {
+		if (index % (MAX / 100) == 0) {
+			cstring *s = new_cstring();
+			cstring_adds(s, "Conversation index step count: ");
+			cstring_addi(s, index / (MAX / 100));
+			display_conversation(s->string);
+			free_cstring(s);
+		}
+
+		str = display_driver();
+		if (str != NULL) {
+			display_debug("SEND: ", 0);
+			display_debug(str, 1);
+		}
+		index++;
+	}
+	return 0;
+}
