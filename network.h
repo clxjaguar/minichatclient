c11501874bcbfc6fcf1334a159e306ab90ee1a5b
cLx <clx.kat@gmail.com>
2013-03-17 22:38:15 +0000

    Larges changes in const char/char types to tidying stuffs. Please check other display plugins to conform to display_interfaces.h !

--- network.h
+++ network.h
@@ -1,7 +1,7 @@
 void ws_init(void);
 void ws_cleanup(void);
-int maketcpconnexion(char* hostname, int port);
-int sendstr(int s, char* buf);
-int sendline(int s, char* buf);
-int http_get(int s, char* req, char* host, char* referer, char* cookies, char* useragent, char *mischeaders);
-int http_post(int s, char* req, char* host, char* datas, char* referer, char* cookies, char* useragent, char* mischeaders);
+int maketcpconnexion(const char* hostname, unsigned int port);
+int sendstr(int s, const char* buf);
+int sendline(int s, const char* buf);
+int http_get(int s, const char* req, const char* host, const char* referer, const char* cookies, const char* useragent, const char *mischeaders);
+int http_post(int s, const char* req, const char* host, const char* datas, const char* referer, const char* cookies, const char* useragent, const char* mischeaders);
