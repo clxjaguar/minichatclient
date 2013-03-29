c3a4cc7cef013e2f8dbde0dda016ba308ec67bc8
clxfr <clxfr@caf73ff4-54bb-40f4-97cd-72ea6ff03f61>
2013-03-29 19:59:31 +0000

    exiting message in statusbar

--- strfunctions.c
+++ strfunctions.c
@@ -24,13 +24,13 @@ void strrep(const char *input, char **out, const char *old, const char *new) {
 
 	found = strstr(in, old);
 	if(!found) {
-		if (input) {
+		//if (input) {
 			if (*out){
-				free(*out); *out = NULL;
+				//free(*out); *out = NULL;
 			}
 			*out = malloc(strlen(in) + 1);
 			strcpy(*out, in);
-		}
+		//}
 		return;
 	}
 
