a4972c49eaead325e41edd583a5654340710d073
cLx <clx.kat@gmail.com>
2014-02-05 16:38:32 +0000

    Code clean-up, some UTF8ing in comments and some preliminary work for nicklist.c

--- nicklist.c
+++ nicklist.c
@@ -1,25 +1,58 @@
 /*
-	Ce module doit gérer la liste des personnes présentes sur le minichat.
+	Ce module doit gÃ©rer la liste des personnes prÃ©sentes sur le minichat.
 
-	<cLx> ça te dirait qu'on se fasse un module qui s'appelle nicklist.c, qui va prendre les appels de parsehtml.c, les requetes de whois de mccirc et qui va aussi fournir la nicklist pour gotcurses.c ?
-	<niki> ça serait bien
-	<cLx> btw, a chaque message, ça va appeler une fonction dans nicklist.c pour mettre à jour l'url de l'icone, du profil, le time du dernier message dit ...
-	<cLx> si une personne est invisible mais parle quand même, on va quand même l'ajouter à la liste d'ailleurs avec un flag pour dire qu'elle est invisible et qu'il faudra compter sur un timeout depuis le dernier message pour la virer de la liste ou pas
+	<cLx> Ã§a te dirait qu'on se fasse un module qui s'appelle nicklist.c, qui va prendre les appels de parsehtml.c, les requetes de whois de mccirc et qui va aussi fournir la nicklist pour gotcurses.c ?
+	<niki> Ã§a serait bien
+	<cLx> btw, a chaque message, Ã§a va appeler une fonction dans nicklist.c pour mettre Ã  jour l'url de l'icone, du profil, le time du dernier message dit ...
+	<cLx> si une personne est invisible mais parle quand mÃªme, on va quand mÃªme l'ajouter Ã  la liste d'ailleurs avec un flag pour dire qu'elle est invisible et qu'il faudra compter sur un timeout depuis le dernier message pour la virer de la liste ou pas
 
 	elements de la "base de donnees" :
 		- nickname
-		- l'url de son icône (récupérée quand la personne parle par msg->usericonurl)
-		- l'url du profil (récupérée quand la personne parle par msg->userprofileurl)
-		- une petite chaine pour y mettre un ident dedans (pour etre utilisée comme mask après)
-		- un timestamp qui indique la date et l'heure d'ajout à la nicklist
+		- l'url de son icÃ´ne (rÃ©cupÃ©rÃ©e quand la personne parle par msg->usericonurl)
+		- l'url du profil (rÃ©cupÃ©rÃ©e quand la personne parle par msg->userprofileurl)
+		- une petite chaine pour y mettre un ident dedans (pour etre utilisÃ©e comme mask aprÃ©s)
+		- un timestamp qui indique la date et l'heure d'ajout Ã  la nicklist
 		- un timestamp qui indique la date et l'heure du dernier message de la personne
-		- un timestamp qui indique la date et l'heure de la dernière fois qu'on a vu la personne dans la liste de gens sur le minichat
-		- un tag qui indique si la personne a été rajoutée à la nicklist sans qu'elle apparaisse dans la liste (ie: elle est invisible) ? En option, puisqu'il suffit de vérifier si le timestamp précédent est à 0 ou pas en fait...
+		- un timestamp qui indique la date et l'heure de la derniÃ¨re fois qu'on a vu la personne dans la liste de gens sur le minichat
+		- un tag qui indique si la personne a Ã©tÃ© rajoutÃ©e Ã  la nicklist sans qu'elle apparaisse dans la liste (ie: elle est invisible) ? En option, puisqu'il suffit de vÃ©rifier si le timestamp prÃ©cÃ©dent est Ã  0 ou pas en fait...
 */
 
+#include <string.h>
+#include <time.h>
+
+#include "nicklist.h"
 #include "display_interfaces.h"
 #include "mccirc.h"
-#include "main.h" //get_mccirc()
+#include "main.h" //malloc_globalise_url(), get_mccirc()
+#include "strfunctions.h"
+
+typedef struct {
+	char *username;
+	char *ident;
+	char *profil_url;
+	char *icon_url;
+	time_t added;
+	time_t lastmessage;
+} tlist;
+
+void nicklist_init(void) {
+}
+
+void nicklist_destroy(void) {
+}
+
+void nicklist_msg_update(const char *username, const char *profil_url, const char *icon_url) {
+	char *profil_url2, *icon_url2;
+
+	icon_url2 = malloc_globalise_url(icon_url);
+	profil_url2 = malloc_globalise_url(profil_url);
+
+	display_debug(profil_url2, 0);
+	display_debug(icon_url2, 0);
+
+	free(profil_url2);
+	free(icon_url2);
+}
 
 void nicklist_recup_start(void) {
 	display_nicklist(NULL);
@@ -27,7 +60,6 @@ void nicklist_recup_start(void) {
 }
 
 void nicklist_recup_end(void) {
-	//display_nicklist("--"); // to test if the fonction is properly called !
 	mccirc_nicks_stop(get_mccirc());
 }
 
--- nicklist.h
+++ nicklist.h
@@ -1,3 +1,6 @@
+void nicklist_init(void);
+void nicklist_destroy(void);
+void nicklist_msg_update(const char *username, const char *profil_url, const char *icon_url);
 void nicklist_recup_start(void);
 void nicklist_recup_end(void);
 void nicklist_recup_name(const char* nickname);
