3c55258b23d92567df9747001352afbdd6b04edf
clxfr <clxfr@caf73ff4-54bb-40f4-97cd-72ea6ff03f61>
2014-04-06 18:38:25 +0000

    New option available for enabling or disabling the IRC AWAY message reporting if the user is considered active or not.

--- ircserver.h
+++ ircserver.h
@@ -1,6 +1,7 @@
 int irc_init(const char *host, const char *port, const char *fakehost, const char *channel_name, const char *forum_username);
 const char* irc_driver(void);
 int irc_destroy(void);
+void irc_set_report_away(int value);
 void irc_join(const char *nickname, const char *ident);
 void irc_part(const char *nickname, const char *ident, const char *partmsg);
 void irc_message(const char *nickname, const char *ident, const char *message);
